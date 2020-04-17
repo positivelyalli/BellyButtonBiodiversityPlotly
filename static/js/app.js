3c2b852d4a45332ccc39718bd8afdd5c3c9d3bde
Alli Kruger <corinne.alli.kruger@gmail.com>
2020-04-16 19:58:02 -0700

    Dropdown working with Names Array data.

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,5 @@
 
-// /**
-//  * Helper function to select sample data
-//  * Returns an array of values
-//  * @param {array} rows
-//  * @param {integer} index
-//  * index 0 - id
-//  * index 1 - otu_ids
-//  * index 2 - sample_values
-//  * index 3 - otu_labels
-//  */
 
-// function unpack(rows, index) {
-//     return rows.map(function(row) {
-//       return row[index];
-//     });
-//   }
-
-  
-// Assign variables to samples.json
-// Read sample.json
-
-// function getOtuData() {
-//     var queryUrl = 'samples.json';
-//     d3.json(queryUrl).then(function(data) {
-//         var sample_values = unpack(data.samples.samples_values);
-//         var otu_ids = unpack(data.samples.otu_ids);
-//         var otu_labels = unpack(data.samples.otu_labels);
-        
-//     });
-// }
 var otuData = {};
 var metadata = {};
 var nameIndex = [];
@@ -38,8 +9,8 @@ function fetchData() {
     d3.json(queryUrl).then(function(data) {
         otuData = getOtuData(data);
         // metadata = getMetaData(data);
-        nameIndex = getNameIndex(data);
-
+        nameIndex = getNames(data);
+        createDropDown(nameIndex);
         // Old Code
     console.log("fetch completed")
     });
@@ -58,41 +29,21 @@ function getOtuData(data) {
         "otu_ids": otu_ids,
         "otu_labels": otu_labels
     };
-};
+}
 
-// function getMetaData(data) {
-//     var metaData_id = data.metadata.id;
-//     var ethnicity = data.metadata.ethnicity;
-//     var gender = data.metadata.gender;
-//     var age = data.metadata.age;
-//     var location = data.metadata.location;
-//     var bbtype = data.metadata.bbtype;
-//     var wfreq = data.metadata.wfreq;
-
-//     // DOES NOT DESTROY REFERENCES
-//     /*
-//     var meta = data.metadata;
-//     meta.metaData_id = data.metadata.id;
-//     meta.wfreq = data.wfreq.wfreq;
-//     */
-//    console.log("getMetaData completed");
-
-//     return {
-//         "metaData_id": metaData_id,
-//         "ethnicity": ethnicity,
-//         "gender": gender,
-//         "age": age,
-//         "location": location,
-//         "bbtype": bbtype,
-//         "wfreq": wfreq
-//     };
-// }
+function getNames(data) {
+    return data.names;
+}
 
 
 // Populate the dropdown with names array
-function createDropDown() {
+function createDropDown(ids) {
     // select dropdown by id
     var dropdown = d3.select("#selDataset");
+    ids.forEach(function (item, index){
+        addDropdownOption(item, item);
+    });
+
 }
 
 function addDropdownOption(optionValue, optionText) {
@@ -104,12 +55,15 @@ function addDropdownOption(optionValue, optionText) {
             .attr('value', optionValue) // edit the option attribute
             .text(optionText);          // edit the option text
 
-    // @todo: return a reference to option
+    // @todo:
+    // return a reference to option
 }
 
 function test() {
-    addDropdownOption(75, "75");
-}
+    fetchData();
+}   
+
+test();
 
 // @TODO Bar Chart
 // samples_values as values for bar_chart
