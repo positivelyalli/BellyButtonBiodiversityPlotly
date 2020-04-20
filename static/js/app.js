f604ad401ed0308f03ac020da521f53a44b3dd69
Alli Kruger <corinne.alli.kruger@gmail.com>
2020-04-19 20:51:14 -0700

    getOtuData is working and createBarChart is working.

--- static/js/app.js
+++ static/js/app.js
@@ -12,41 +12,57 @@ function fetchData() {
         nameIndex = getNames(data);
         createDropDown(nameIndex);
 
-        // get filtered data:
+        // FilteredOtuData
         // var selectedID = d3.select(#drop-down)
         // var filteredData = ....filter(selectedID =>)
 
         // Populate Metadata
         // keep in mind key-value pairs
         // createMetaData(filteredData)
-        getMetaData(data);
+        updateDemoInfo(data.metadata[0]);
 
         
       
         // plots data
         otuData = getOtuData(data);
+        console.log(otuData);
     console.log("fetch completed")
     });
 }
 
-function getFilteredData(data, inputValue) {
-    var filteredData = data.metadata.filter(nameID => nameID.id == inputValue);
+function getFilteredData(data, selectedSubjectID) {
+    var filteredData = data.metadata.filter(function(nameID){
+        return nameID.id == inputValue; // there is an id that is an integer and one is a string
+    });
     console.log(filteredData);
     return filteredData;
 }
 
+
+
 function getOtuData(data) {
+    // path for json data object
     var queryUrl = 'samples.json';
+    // get json data object
     d3.json(queryUrl).then(function(data) {
-        var sample_values = data.samples.sample_values;
-        console.log(sample_values);
-
-        var otu_ids = data.samples.otu_ids;
+        // Sample value data from the first index of samples
+        var sample_values = data.samples[0].sample_values;
+        console.log("sample values");
+        // Print sample value data from the first index of samples
+        console.log(data.samples[0].sample_values)
+        
+        // OTU ids from the first index of samples
+        var otu_ids = data.samples[0].otu_ids;
+        // Print OTU ids from the first index of samples
         console.log(otu_ids);
-
-        var otu_labels = data.samples.otu_labels; 
+        
+        // OTU labels from the first index of samples
+        var otu_labels = data.samples[0].otu_labels;
+        // Print OTU labels from the first index of samples 
         console.log(otu_labels);
-        return {
+
+        // Return a js object of the data to use in the charts
+        otuData = {
             "sample_values": sample_values,
             "otu_ids": otu_ids,
             "otu_labels": otu_labels
@@ -54,24 +70,81 @@ function getOtuData(data) {
     });
 }
 
+// Data filtered by top 10 and sorted descending
+function filteredOtuData(otuData) {
+    var x = [163, 126, 113, 78, 71, 51, 50, 47, 40, 40];
+    var y = [
+        'OTU 1167', 
+        'OTU 2859', 
+        'OTU 482', 
+        'OTU 2264', 
+        'OTU 41', 
+        'OTU 1189', 
+        'OTU 352', 
+        'OTU 189', 
+        'OTU 2318', 
+        'OTU 1977'
+    ];
+    var text = [
+        "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", 
+        "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", 
+        "Bacteria", 
+        "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", 
+        "Bacteria", 
+        "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", 
+        "Bacteria", 
+        "Bacteria", 
+        "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", 
+        "Bacteria;Firmicutes;Clostridia;Clostridiales"
+    ];
+    return {
+        "x": x,
+        "y": y,
+        "text": text
+    }
+}
+
+// Retrieves an array of name ids from the json file
 function getNames(data) {
     return data.names;
 }
 
 
-
-function getMetaData(data) {
+function js_getMetaData(data) {
     var dataTable = d3.select("#sample-metadata");
     Object.entries(data).forEach(([key, value]) => {
-        var li = dataTable.append('li').text(`${key}, ${value}`);
+        dataTable.append('li').text(`${key}, ${value}`);
     });
 }
- 
-function getMetaData(data) {
+
+// Creates a Demographic Chart from a 'Data' object,
+// primarily uses data.metadata as source
+function createDemoChart(data) {
     var dataTable = d3.select("#sample-metadata");
-    data.forEach(function(item, index) {
-        dataTable.append('li').text(item, item);
+
+    // itterates through the metadata array
+    data.metadata.forEach(function(item, index) {
+
+        // converts the item object to a string
+        // var text = `${index}, ${item} \t\t`;
+        var text = '';
+
+        Object.keys(item).forEach(function(key){
+            text += `${key} :: ${item[key]}, \t`;
+        });
+
+        // appends the completed item to the array
+        dataTable.append('li').text(text);
     })
+}
+
+// takes a metadata item and renders the demographic info box
+function updateDemoInfo(metadataItem){
+    var dataTable = d3.select("#sample-metadata");
+
+    Object.keys(metadataItem).forEach(function(key){
+        dataTable.append('li').text( `${key}: ${metadataItem[key]}`)
+    });
 }
 
 
@@ -118,7 +191,7 @@ test();
 // }
 
 function optionChanged(nameID) {
-
+    console.log("optionChanged", nameID);
     filteredData = getFilteredData(nameID);
     getMetaData(filteredData);
 
@@ -133,34 +206,39 @@ function optionChanged(nameID) {
 
 // otu_ids as labels for bar chart
 
-// otu_lables as the hovertext for the bar chart
+// otu_lables as the hovertext for the bar chart\
 
-// function createBarPlot() {
+function test_createBarPlot() {
+    var barChartData = filteredOtuData(otuData);
+    createBarPlot(barChartData);
+}
+
+function createBarPlot(sample_data) {
     // d3.json(queryUrl).then(function(data){
-        var sample_values
-        
-        var trace1 = {
-            //   x: otuData.samples.sample_values,
-            //   y: otuData.samples.otu_ids,
-                x: ["beer", "wine", "martini", "margarita",
-                "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-                y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],   
-                type: "bar",
-                // text: otuData.samples.otu_labels
-            };
-        
-        var data = [trace1];
-        
-        var layout = {
-        title: "'Bar' Chart",
-        xaxis: { title: "Drinks"},
-        yaxis: { title: "% of Drinks Ordered"}
+    //     var sample_values = data.samples.sample_values
+    
+    
+    var trace1 = {
+        //   x: otuData.samples[0].sample_values,
+        //   y: otuData.samples[0].otu_ids,
+        x: sample_data.x,
+        y: sample_data.y,   
+        type: "bar",
+        text: sample_data.text,
+        orientation: 'h'
         };
     
-        Plotly.newPlot("bar", data, layout);
+    var data = [trace1];
+    
+    var layout = {
+    title: "Top 10 OTU Chart",
+    xaxis: { title: "Sample Values"}
+    };
+
+    Plotly.newPlot("bar", data, layout);
     // });
 
-// }
+}
 
 
 
